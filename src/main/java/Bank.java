9f34bb2cc959fd23bfe4f0114f220c1769616f5f
takumasetoguchi <setoguchi.takuma@gmail.com>
2015-11-10 14:18:56 +0900

    Bank.reduce(Money)に対応

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,6 +1,7 @@
 class Bank {
 
     Expression reduce(Expression source, String to) {
+        if (source instanceof Money) return ((Money) source).reduce(to);
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
